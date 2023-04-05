0e02aeceba11dc3b8c5105cd14692bb8fe5a66ee
grin <grin@kth.se>
2023-04-05 21:58:36 +0200

    palinda 3

--- palinda-3/mutexExerciseSkal.go
+++ palinda-3/mutexExerciseSkal.go
@@ -10,25 +10,25 @@ type bankAccount struct {
 	balance int
 }
 
-func incrementBalance(s *bankAccount, wg *sync.WaitGroup, mtx *sync.Mutex) {
-	mtx.Lock()
+
+func incrementBalance(s *bankAccount, wg *sync.WaitGroup) {
 	(*s).balance = (*s).balance - 1
-	mtx.Unlock()
-	wg.Done()
+	// TODO (Do something important on this line)
 }
 
 /*
-* For each Goroutine we want to decrement the balance
-* by 1.
+* For each Goroutine we want to increment the car's
+* mileage by 1.
+* In total, we want the mileage to be set to 2000,
+* however, something is wrong...
  */
 func main() {
 	numOfGoroutines := 1000
 	myAccount := bankAccount{"Handelsbanken", 1000}
 	var w sync.WaitGroup
-	var m sync.Mutex
 	for i := 0; i < numOfGoroutines; i++ {
-		w.Add(1)
-		go incrementBalance(&myAccount, &w, &m)
+		// TODO (Do something important on this line)
+		go incrementBalance(// TODO)
 	}
 	w.Wait()
 	fmt.Println(myAccount.balance)
